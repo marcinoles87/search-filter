2e112efad1060917f5853fcfa42e26ee236bd705
unknown <marcinoles87@gmail.com>
2023-02-14 17:44:25 +0100

    add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import Search from './components/Search.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Search Filter</h1>
+      <Search></Search>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Search = () => {
+    return ( 
+        <div>
+            <input placeholder="search tag..."></input>
+        </div>
+     );
+}
+ 
+export default Search;
