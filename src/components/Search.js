0b1f81fd898774c921daba32adf90f265ed4e127
unknown <marcinoles87@gmail.com>
2023-02-15 15:47:32 +0100

    add function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import JSONDATA from '../Data.json'
+
+import './search.css'
+
 
 
 const Search = () => {
+
+    let [text,handleText] = useState('');
+
+    handleText = (e) => {
+        let word = e.target.value
+        console.log(word)
+
+    }
+
     return ( 
-        <div>
-            <input  placeholder="search tag..."></input>
+        <div className='search-wrapper'>
+            <input onChange={handleText} placeholder="search tag..."></input>
             {JSONDATA.map((val,key)=>{
                 return <div>
-                    <li key={val.nick}>Name: {val.name} nick :{val.nick}</li></div>
+                    <li key={key.nick}>Name: {val.name} nick :{val.nick}</li></div>
 
             })}
         </div>
